9728609109a47d8d5707270fa525d6d709b03327
Detlev Casanova <detlev.casanova@collabora.com>
2023-08-17 09:56:59 -0400

    wip-test-loaded

--- test-cases/run_tests.sh
+++ test-cases/run_tests.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -x
+
 # Run tests for cros-codecs using ccdec and flsuter.
 
 # Arguments:
@@ -28,6 +30,22 @@ SKIP_VECTORS_vp9_test_vectors="vp90-2-22-svc_1280x720_3.ivf vp91-2-04-yuv422.web
 SKIP_VECTORS_jvt_avc_v1="CVFC1_Sony_C FM1_BT_B FM1_FT_E FM2_SVA_C MR5_TANDBERG_C MR8_BT_B MR9_BT_B SP1_BT_A sp2_bt_b"
 SKIP_VECTORS_jct_vc_hevc_v1="CONFWIN_A_Sony_1 RAP_B_Bossen_2 RPS_C_ericsson_5 RPS_E_qualcomm_5 TSUNEQBD_A_MAIN10_Technicolor_2"
 
+PARTITION="/dev/nvme0n1p1"
+# Load vectors on hard drive
+if mountpoint /mnt/vectors; then
+	echo "Already loaded"
+fi
+
+mkdir -p /mnt/vectors
+mount ${PARTITION} /mnt/vectors
+time cp -a /opt/fluster/resources/ /mnt/vectors/
+rm -fr /opt/fluster/resources
+ln -s /mnt/vectors/resources /opt/fluster/resources
+
+export LIBVA_DRIVERS_PATH=/mnt/vectors/
+cp -a /usr/lib/x86_64-linux-gnu/dri/iHD_drv_video.so /mnt/vectors/
+
+#  Download ccdec binary
 if [ ! -e /opt/cros-codecs/ccdec ]; then
 	mkdir /opt/cros-codecs
 	cd /opt/cros-codecs
@@ -37,6 +55,8 @@ if [ ! -e /opt/cros-codecs/ccdec ]; then
 fi
 
 export PATH=$PATH:/opt/cros-codecs
+
+time ccdec /opt/fluster/resources/VP9-TEST-VECTORS/vp90-2-14-resize-10frames-fp-tiles-8-1.webm/vp90-2-14-resize-10frames-fp-tiles-8-1.webm --output /dev/null --input-format vp9 --output-format i420
 
 if [ "${SINGLE_RUN}" == "yes" ]; then
 	FLUSTER_ARGS="-j 1"
